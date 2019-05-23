6501336d9ada73b3f98ef76d301de033a19b1186
Leo Valais <leo.valais97@gmail.com>
2019-05-23 19:37:32 +0200

    add: 3D hilbert curve L-System

--- examples.lisp
+++ examples.lisp
@@ -73,6 +73,17 @@
 (define-rule A () #i(noop) #wF[&B][^B][+B][-B])
 (define-rule B () #i(noop) #wFA)
 
+(define-lsystem *3d-hilbert* #wA)
+(define-3d-turtle (/ pi 2))
+(define-rule ! () #i(yaw pi)) ; turn around
+(define-rule A () #i(noop)
+  #wB-F+CFC+F-D&F^D-F+&&CFC+F+B>>)
+(define-rule B () #i(noop)
+  #wA&F^CFB^F^D^^-F-D^!F^B!FC^F^A>>)
+(define-rule C () #i(noop)
+  #w!D^!F^B-F+C^F^A&&FA&F^C+F+B^F^D>>)
+(define-rule D () #i(noop)
+  #w!CFB-F+B!FA&F^A&&FB-F+B!FC>>)
 
 (define-lsystem *param-1.7* #w((B 2) (A 4 4)))
 (define-rule A (x y) #i(forward (+ x y))
