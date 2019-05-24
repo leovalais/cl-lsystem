bf1fb79758d0c5dde6c9e45848c7b3fb4383ffe2
Leo Valais <leo.valais97@gmail.com>
2019-05-25 01:57:05 +0200

    add: Dockerfile
    misc:rm: useless Dockerfile step
    
    misc: makes asdf:load-system not verbose for docker run

---
+++ .dockersetup.lisp
@@ -0,0 +1,13 @@
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+
+(setf sb-impl::*default-external-format* :utf-8)
+(setf sb-alien::*default-c-string-external-format* :utf-8)
+
+(dolist (asd (rest *posix-argv*))
+  (with-open-file (in asd)
+    (let* ((system (cddr (read in)))
+           (deps (getf system :depends-on)))
+      (mapc #'ql:quickload deps))))
