0964a674d3fba56032c967bf6cd8845216b25ba7
Leo Valais <leo.valais97@gmail.com>
2019-05-28 18:52:02 +0200

    fix: add evaluator function for env variables in define-env-fun

--- scripting.lisp
+++ scripting.lisp
@@ -1,6 +1,5 @@
 (in-package :cl-lsystem/scripting)
 
-
 (defmacro define-env-fun (name (&rest arguments) &body body)
   (let* ((vars (mapcar (lambda (arg)
                          (etypecase arg
@@ -13,28 +12,36 @@
                            (list (or (second arg)
                                      nil))))
                        arguments))
+         (evls (mapcar (lambda (arg)
+                         (etypecase arg
+                           (symbol 'read-from-string)
+                           (list (or (third arg)
+                                     'read-from-string))))
+                       arguments))
          (envs (mapcar (lambda (arg)
                          (etypecase arg
                            (symbol (symbol-name arg))
-                           (list (or (third arg)
+                           (list (or (fourth arg)
                                      (symbol-name (first arg))))))
                        arguments)))
     `(defun ,name (&key ,@(mapcar (lambda (var def)
                                     (list var def))
                                   vars defs))
-       ,@(mapcar (lambda (var env)
+       ,@(mapcar (lambda (var env evaluator)
                    (let ((varenv (gensym (concatenate 'string "ENV-" (symbol-name var)))))
                      `(let ((,varenv (sb-unix::posix-getenv ,env)))
                         (when ,varenv
-                          (setf ,var ,varenv)))))
-                 vars envs)
+                          (setf ,var
+                                ,(if (symbolp evaluator)
+                                     `(funcall #',evaluator ,varenv)
+                                     `(funcall ,evaluator ,varenv)))))))
+                 vars envs evls)
        ,@body)))
-
 
 (define-env-fun png ((lsystem *lsystem*)
                      (n 0)
-                     (out "out")
-                     (origin (v 0.0 0.0))
+                     (out "out" identity)
+                     (origin (v 0.0 0.0) (lambda (s) (eval (read-from-string s))))
                      (width 500)
                      (height 500)
                      (direction (v 1.0 0.0)))
